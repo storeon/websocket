8f98bebf173fe07a8262b338bf1d49dc7cb2a434
polemius <ivan.menshykov@gmail.com>
2020-01-02 16:58:26 +0100

    Add types for typescript

---
+++ index.d.ts
@@ -0,0 +1,18 @@
+import createStore from 'storeon'
+
+/**
+ *  Storeon module to send and receive events by WebSocket
+ *  @param {String} url The url of WebSocket server
+ *  @param {String[]} include The array that described what event
+ *      should be sent/dispatched
+ *  @param {Number} reconnectInterval Interval after trying to reconnect
+ *  @param {Number} pingPongInterval Interval to send 'ping' to server
+ **/
+declare module StoreonWebsocket {
+    export function websocket<State>(
+        url: string,
+        include?: String[],
+        reconnectInterval?: Number,
+        pingPongInterval?: Number
+    ): createStore.Module<State>
+}
